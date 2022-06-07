d0f1d2485aec615ee408ff809ae1b82a72bd1ecd
Mohit <mohitxsh@gmail.com>
2022-06-07 23:33:13 +0530

    some changes

--- client/src/components/routing/PrivateRoute.js
+++ client/src/components/routing/PrivateRoute.js
@@ -10,7 +10,7 @@ function PrivateRoute({ component: Component, ...rest }) {
     <Route
       {...rest}
       render={(props) =>
-        !isAuthenticated && !loading ? (
+        !isAuthenticated && loading ? (
           <Redirect to='/login' />
         ) : (
           <Component {...props} />
--- client/src/context/auth/AuthState.js
+++ client/src/context/auth/AuthState.js
@@ -33,7 +33,7 @@ const AuthState = (props) => {
     }
 
     try {
-      const res = await axios.get("/api/auth");
+      const res = await axios.get("http://localhost:5000/api/auth");
       dispatch({ type: USER_LOADED, payload: res.data });
     } catch (err) {
       dispatch({ type: AUTH_ERROR });
@@ -48,7 +48,11 @@ const AuthState = (props) => {
       },
     };
     try {
-      const res = await axios.post("/api/users", formData, config);
+      const res = await axios.post(
+        "http://localhost:5000/api/users",
+        formData,
+        config
+      );
 
       dispatch({
         type: REGISTER_SUCCESS,
@@ -71,7 +75,11 @@ const AuthState = (props) => {
       },
     };
     try {
-      const res = await axios.post("/api/auth", formData, config);
+      const res = await axios.post(
+        "http://localhost:5000/api/auth",
+        formData,
+        config
+      );
 
       dispatch({
         type: LOGIN_SUCCESS,
@@ -87,7 +95,9 @@ const AuthState = (props) => {
   };
 
   //Logout User
-  const logout = () => {dispatch({ type: LOGOUT })};
+  const logout = () => {
+    dispatch({ type: LOGOUT });
+  };
 
   //Clear Errors
 
@@ -109,6 +119,5 @@ const AuthState = (props) => {
     </AuthContext.Provider>
   );
 };
-//every component wrapped in the contactstate component now has access to the state.contacts or any method defined in value
 
 export default AuthState;

51f87a9d8919040f151152f9f9f52abd53f99987
Mohit <mohitxsh@gmail.com>
2022-06-09 11:27:44 +0530

    changes

--- server.js
+++ server.js
@@ -10,18 +10,17 @@ connectDB();
 
 //init Middleware
 app.use(express.json({ extended: false }));
-
-app.get("/api", (req, res) =>
-  res.json({ msg: "Welcome to the movie library API" })
-);
-
+const PORT = process.env.PORT || 5000;
 //routes
 app.use("/api/users", require("./routes/users"));
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/watchlist", require("./routes/watchlist"));
 app.use(express.static(path.join(__dirname, "client", "build")));
-const PORT = process.env.PORT || 5000;
+
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 });
+app.get("/api", (req, res) =>
+  res.json({ msg: "Welcome to the movie library API" })
+);
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
